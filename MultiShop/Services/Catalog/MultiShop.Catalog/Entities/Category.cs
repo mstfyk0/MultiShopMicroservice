849bb640b79cbc6e8b9a5aa6c41bc427fcf13a53
Mustafa Yiğit KARAKOCA <ygt99krk@gmail.com>
2024-04-21 01:03:30 +0300

    created a new entites class.

--- MultiShop/Services/Catalog/MultiShop.Catalog/Entities/Category.cs
+++ MultiShop/Services/Catalog/MultiShop.Catalog/Entities/Category.cs
@@ -1,6 +1,20 @@
-﻿namespace MultiShop.Catalog.Entities
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace MultiShop.Catalog.Entities
 {
     public class Category
     {
+
+        /// <summary>
+        /// Bu alanın id olduğunu söylemek için 
+        /// [BsonId]
+            ///[BsonRepresentation(BsonType.ObjectId)] kodlarını kullanıyoruz.
+        /// </summary>
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public  string CategoryId  { get; set; }
+
+        public string CategoryName { get; set; }    
     }
 }
