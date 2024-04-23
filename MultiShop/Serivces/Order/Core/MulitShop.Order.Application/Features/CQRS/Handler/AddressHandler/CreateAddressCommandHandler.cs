d7269c426717f7053347e17c35064c16c0291935
Mustafa Yiğit KARAKOCA <ygt99krk@gmail.com>
2024-04-23 10:22:15 +0300

    Implementated for create handler.

---
+++ MultiShop/Serivces/Order/Core/MulitShop.Order.Application/Features/CQRS/Handler/AddressHandler/CreateAddressCommandHandler.cs
@@ -0,0 +1,22 @@
+﻿using MulitShop.Order.Application.Interfaces;
+using MultiShop.Order.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MulitShop.Order.Application.Features.CQRS.Handler.AddressHandler
+{
+    public class CreateAddressCommandHandler
+    {
+        private readonly IRepository<Address> _repository;
+
+        public CreateAddressCommandHandler(IRepository<Address> repository)
+        {
+            _repository = repository;
+        }
+
+
+    }
+}
