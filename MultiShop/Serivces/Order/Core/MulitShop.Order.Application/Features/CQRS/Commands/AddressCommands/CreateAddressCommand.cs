815b59d29305a4977d3f6a4cded78813dc47f668
Mustafa Yiğit KARAKOCA <ygt99krk@gmail.com>
2024-04-23 10:18:24 +0300

    Added new microservice of order.

---
+++ MultiShop/Serivces/Order/Core/MulitShop.Order.Application/Features/CQRS/Commands/AddressCommands/CreateAddressCommand.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MulitShop.Order.Application.Features.CQRS.Commands.AddressCommands
+{
+    public class CreateAddressCommand
+    {
+    }
+}
---
+++ MultiShop/Serivces/Order/Core/MulitShop.Order.Application/Features/CQRS/Querys/AddresQuerys/GetAddressByIdQuery.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MulitShop.Order.Application.Features.CQRS.Querys.AddresQuerys
+{
+    public class GetAddressByIdQuery
+    {
+        public int Id { get; set; }
+
+        public GetAddressByIdQuery(int id)
+        {
+            Id = id;
+        }
+    }
+}
