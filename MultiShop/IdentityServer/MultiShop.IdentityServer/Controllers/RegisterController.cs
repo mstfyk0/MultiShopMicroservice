6f9b947caf432941a57dfcf7c8dfd56ba91b2599
Mustafa Yiğit KARAKOCA <ygt99krk@gmail.com>
2024-04-25 22:07:30 +0300

    Added Identity server and register controller.

---
+++ MultiShop/IdentityServer/MultiShop.IdentityServer/Controllers/RegisterController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using MultiShop.IdentityServer.Dtos;
+using MultiShop.IdentityServer.Models;
+using System.Threading.Tasks;
+
+namespace MultiShop.IdentityServer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RegisterController : ControllerBase
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public RegisterController(UserManager<ApplicationUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> UserRegister(UserRegisterDto userRegisterDto)
+        {
+            var values = new ApplicationUser
+            {
+                UserName = userRegisterDto.Username,
+                Email = userRegisterDto.Email,
+                Name = userRegisterDto.Name,
+                SurName = userRegisterDto.Surname
+            };
+            var result = await _userManager.CreateAsync(values,userRegisterDto
+                .Password);
+
+            if (result.Succeeded)   
+            {
+                return Ok("Kullanıcı başarıyla oluşturuldu.");
+            }
+
+            return Ok("Bir hata oluştu tekrar deneyiniz.");
+        }
+    }
+}
