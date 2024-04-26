373a9db8edbe898b1fbb06d237b18fe18c847bd6
Mustafa Yiğit KARAKOCA <ygt99krk@gmail.com>
2024-04-26 10:59:22 +0300

    Added IdentityServer4 and register controller protection

--- MultiShop/IdentityServer/MultiShop.IdentityServer/Config.cs
+++ MultiShop/IdentityServer/MultiShop.IdentityServer/Config.cs
@@ -2,6 +2,7 @@
 // Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
 
 
+using IdentityServer4;
 using IdentityServer4.Models;
 using System.Collections.Generic;
 
@@ -9,50 +10,86 @@ namespace MultiShop.IdentityServer
 {
     public static class Config
     {
-        public static IEnumerable<IdentityResource> IdentityResources =>
-                   new IdentityResource[]
-                   {
-                new IdentityResources.OpenId(),
-                new IdentityResources.Profile(),
-                   };
-
-        public static IEnumerable<ApiScope> ApiScopes =>
-            new ApiScope[]
+        public static IEnumerable<ApiResource> ApiResources => new ApiResource[]
+        {
+            new ApiResource("ResourceCatalog")
             {
-                new ApiScope("scope1"),
-                new ApiScope("scope2"),
-            };
+                Scopes={"CatalogFullPermission","CatalogReadPermission"}
+            },
 
-        public static IEnumerable<Client> Clients =>
-            new Client[]
+            new ApiResource("ResourceDiscount")
             {
-                // m2m client credentials flow client
-                new Client
-                {
-                    ClientId = "m2m.client",
-                    ClientName = "Client Credentials Client",
+                Scopes={"DiscountFullPermission","DiscountReadPermission"}
+            },
 
-                    AllowedGrantTypes = GrantTypes.ClientCredentials,
-                    ClientSecrets = { new Secret("511536EF-F270-4058-80CA-1C89C192F69A".Sha256()) },
+            new ApiResource("ResourceOrder")
+            {
+                Scopes={"OrderFullPermission","OrderReadPermission"}
+            },
+            new ApiResource(IdentityServerConstants.LocalApi.ScopeName)
 
-                    AllowedScopes = { "scope1" }
+
+        };
+
+        public static IEnumerable<IdentityResource> IdentityResources => new IdentityResource[]
+        {
+            new IdentityResources.OpenId(),
+            new IdentityResources.Email(),
+            new IdentityResources.Profile()
+        };
+
+        public static IEnumerable<ApiScope> ApiScopes => new ApiScope[]
+        {
+            //Full permission
+            new ApiScope("CatalogFullPermission","Full authorization for category operations."),
+            new ApiScope("DiscountFullPermission","Full authorization for discount operations."),
+            new ApiScope("OrderFullPermission","Full authorization for order operations."),
+
+            //Read permission
+            new ApiScope("CatalogReadPermission","Read authorization for category operations."),
+            new ApiScope("DiscountReadPermission","Read authorization for discount operations."),
+            new ApiScope("OrderReadPermission","Read authorization for order operations."),
+            new ApiScope(IdentityServerConstants.LocalApi.ScopeName)
+
+        };
+
+        public static IEnumerable<Client> Clients => new Client[]
+        {
+            //Visiter
+            new Client
+            {
+                ClientId="MultiShopVisiterId",
+                ClientName="Mutli Shop Visiter User",
+                AllowedGrantTypes=GrantTypes.ClientCredentials,
+                ClientSecrets={ new Secret("multishopsecret".Sha256())},
+                AllowedScopes={ "CatalogReadPermission" } 
+            },
+            //Manager
+            new Client
+            {
+                ClientId="MultiShopManagerId",
+                ClientName="Mutli Shop Manager User",
+                AllowedGrantTypes=GrantTypes.ClientCredentials,
+                ClientSecrets={ new Secret("multishopsecret".Sha256())},
+                AllowedScopes={ "CatalogFullPermission" , "CatalogReadPermission" }
+            },
+            //Admin
+            new Client
+            {
+                ClientId="MultiShopAdminId",
+                ClientName="Mutli Shop Admin User",
+                AllowedGrantTypes=GrantTypes.ClientCredentials,
+                ClientSecrets={ new Secret("multishopsecret".Sha256())},
+                AllowedScopes={ "CatalogFullPermission" , "CatalogReadPermission" ,
+                    "DiscountFullPermission","OrderFullPermission"
+                    ,IdentityServerConstants.LocalApi.ScopeName
+                    ,IdentityServerConstants.StandardScopes.Email
+                    ,IdentityServerConstants.StandardScopes.OpenId
+                    ,IdentityServerConstants.StandardScopes.Profile
+
                 },
-
-                // interactive client using code flow + pkce
-                new Client
-                {
-                    ClientId = "interactive",
-                    ClientSecrets = { new Secret("49C1A7E1-0C79-4A89-A3D6-A37998FB86B0".Sha256()) },
-
-                    AllowedGrantTypes = GrantTypes.Code,
-
-                    RedirectUris = { "https://localhost:44300/signin-oidc" },
-                    FrontChannelLogoutUri = "https://localhost:44300/signout-oidc",
-                    PostLogoutRedirectUris = { "https://localhost:44300/signout-callback-oidc" },
-
-                    AllowOfflineAccess = true,
-                    AllowedScopes = { "openid", "profile", "scope2" }
-                },
-            };
+                AccessTokenLifetime=600
+            }
+        };
     }
 }
