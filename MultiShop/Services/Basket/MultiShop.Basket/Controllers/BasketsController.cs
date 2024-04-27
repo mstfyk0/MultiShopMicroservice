22826a9b9e06623c34270c33745bc7d9f2362372
Mustafa Yiğit KARAKOCA <ygt99krk@gmail.com>
2024-04-27 19:05:18 +0300

    Fixed any bugs

--- MultiShop/Services/Basket/MultiShop.Basket/Controllers/BasketsController.cs
+++ MultiShop/Services/Basket/MultiShop.Basket/Controllers/BasketsController.cs
@@ -22,6 +22,7 @@ namespace MultiShop.Basket.Controllers
         [HttpGet]
         public async Task<IActionResult> GetBasketDetail()
         {
+            var user = User.Claims;
             var values = await _basketService.GetBasket(_loginService.GetUserId);
             return Ok(values);
         }
