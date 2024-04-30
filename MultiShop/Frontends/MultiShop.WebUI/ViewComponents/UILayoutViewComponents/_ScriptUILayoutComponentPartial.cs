bbfa4efaa97b4b7b59c3b6c5565cfbce6554b519
Mustafa Yiğit KARAKOCA <ygt99krk@gmail.com>
2024-04-30 08:51:25 +0300

    Added home page view components. Added product page view components

---
+++ MultiShop/Frontends/MultiShop.WebUI/ViewComponents/UILayoutViewComponents/_ScriptUILayoutComponentPartial.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MultiShop.WebUI.ViewComponents.UILayoutViewComponents
+{
+    public class _ScriptUILayoutComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke() {  return View(); }    
+    }
+}
