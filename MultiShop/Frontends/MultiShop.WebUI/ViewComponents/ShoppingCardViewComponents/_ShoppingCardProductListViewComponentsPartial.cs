43a5350f6c4fa20093c396065bec6b7ebdf8e341
Mustafa Yiğit KARAKOCA <ygt99krk@gmail.com>
2024-05-02 12:03:46 +0300

    Added new controller and new view components  to shopping card page

---
+++ MultiShop/Frontends/MultiShop.WebUI/ViewComponents/ShoppingCardViewComponents/_ShoppingCardProductListViewComponentsPartial.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MultiShop.WebUI.ViewComponents.ShoppingCardViewComponents
+{
+    public class _ShoppingCardProductListViewComponentsPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke() { return View(); } 
+
+    }
+}
