95e5bbbec6f28821b2e544edf7a33d4ce9fbf3ac
Mustafa Yiğit KARAKOCA <ygt99krk@gmail.com>
2024-05-02 16:46:19 +0300

    Added shopping details page

---
+++ MultiShop/Frontends/MultiShop.WebUI/ViewComponents/ShoppingCardViewComponents/_ShoppingCardDiscountCouponComponentPartial.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MultiShop.WebUI.ViewComponents.ShoppingCardViewComponents
+{
+    public class _ShoppingCardDiscountCouponComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke() {  return View(); }
+    }
+}
