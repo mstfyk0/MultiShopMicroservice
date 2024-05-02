2d0260b5015cb7a43e7b420b3b7fbd9bc5c50ff1
Mustafa Yiğit KARAKOCA <ygt99krk@gmail.com>
2024-05-02 20:38:17 +0300

    Added admin panel category page

---
+++ MultiShop/Frontends/MultiShop.WebUI/Areas/Admin/Controllers/CategoryController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace MultiShop.WebUI.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CategoryController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public CategoryController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+
+            ViewBag.v0 = "Kategori İşlemleri";
+            ViewBag.v1 = "Ana Sayfa";
+            ViewBag.v2 = "Kategoriler";
+            ViewBag.v3 = "Kategori Listesi";
+
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7047/api/categories");
+            
+
+            if (responseMessage.IsSuccessStatusCode) 
+            {
+                var jsonData= await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<string, object>>(jsonData);
+            }
+
+            return View();
+        }
+    }
+}
