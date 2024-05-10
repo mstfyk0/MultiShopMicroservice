4d4adf5dda52998df79ae447de411834be03555d
Mustafa Yiğit KARAKOCA <ygt99krk@gmail.com>
2024-05-10 23:22:13 +0300

    Changed main page of partial views.

--- MultiShop/Frontends/MultiShop.WebUI/ViewComponents/DefaultViewComponents/_FeatureProductsDefaultComponentPartial.cs
+++ MultiShop/Frontends/MultiShop.WebUI/ViewComponents/DefaultViewComponents/_FeatureProductsDefaultComponentPartial.cs
@@ -1,12 +1,34 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MultiShop.DtoLayer.CatalogDtos.ProductDtos;
+using Newtonsoft.Json;
 
 namespace MultiShop.WebUI.ViewComponents.DefaultViewComponents
 {
     public class _FeatureProductsDefaultComponentPartial : ViewComponent
     {
-        public IViewComponentResult Invoke() 
+
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _FeatureProductsDefaultComponentPartial(IHttpClientFactory httpClientFactory)
         {
-            return View(); 
+            _httpClientFactory = httpClientFactory;
+        }
+        [Route("Index")]
+
+        public async Task<IViewComponentResult> InvokeAsync() 
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7047/api/products/getProductListWithCategory");
+
+
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultProductsWithCategoryDto>>(jsonData);
+                return View(values);
+            }
+
+            return View();
         }
     }
 }
