bc4f8ec78b98afd1fb3c8e76263bd684ee07d45d
Mustafa Yiğit KARAKOCA <ygt99krk@gmail.com>
2024-05-13 14:04:59 +0300

    Implemented for product detail feature

---
+++ MultiShop/Frontends/MultiShop.DtoLayer/CatalogDtos/ProductDtos/GetByIdProductDto.cs
@@ -0,0 +1,13 @@
+﻿
+namespace MultiShop.DtoLayer.CatalogDtos.ProductDtos
+{
+    public record GetByIdProductDto
+    {
+        public string ProductId { get; init; }
+        public string ProductName { get; init; }
+        public decimal ProductPrice { get; init; }
+        public string ProductImageUrl { get; init; }
+        public string ProductDescription { get; init; }
+        public string CategoryId { get; init; }
+    }
+}
--- MultiShop/Frontends/MultiShop.WebUI/ViewComponents/ProductDetailViewComponents/_ProductDetailFeatureComponentPartial.cs
+++ MultiShop/Frontends/MultiShop.WebUI/ViewComponents/ProductDetailViewComponents/_ProductDetailFeatureComponentPartial.cs
@@ -1,9 +1,34 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MultiShop.DtoLayer.CatalogDtos.ProductDtos;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace MultiShop.WebUI.ViewComponents.ProductDetailViewComponents
 {
     public class _ProductDetailFeatureComponentPartial : ViewComponent
     {
-        public IViewComponentResult Invoke() { return View(); }
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _ProductDetailFeatureComponentPartial(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+        public async Task<IViewComponentResult> InvokeAsync(string id) 
+        {
+
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7047/api/Products/getProductById/" + id);
+
+
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<GetByIdProductDto>(jsonData);
+
+                return View(values);
+            }
+            return View();
+
+        }
     }
 }
