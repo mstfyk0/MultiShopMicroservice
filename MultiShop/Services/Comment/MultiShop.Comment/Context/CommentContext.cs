77dcb890b2e7543ad84c43bfc075c4835220421b
Mustafa Yiğit KARAKOCA <ygt99krk@gmail.com>
2024-06-06 19:46:56 +0300

    Implemented for comment api

---
+++ MultiShop/Services/Comment/MultiShop.Comment/Context/CommentContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MultiShop.Comment.Entities;
+
+namespace MultiShop.Comment.Context
+{
+    public class CommentContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server = localhost,1472; Initial Catalog = MultiShopCommentDb; TrustServerCertificate=True; MultipleActiveResultSets=true; User ID = sa; Password=123456aA*;");
+        }
+
+        public DbSet<UserComment> UserCommnets { get; set; }
+
+    }
+}
---
+++ MultiShop/Services/Comment/MultiShop.Comment/Controllers/CommentsController.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using MultiShop.Comment.Context;
+using MultiShop.Comment.Entities;
+
+namespace MultiShop.Comment.Controllers
+{
+    [AllowAnonymous]
+    [Route("api/[controller]")]
+    [ApiController]
+    //[Authorize]
+    public class CommentsController : ControllerBase
+    {
+        private readonly CommentContext _context;
+
+        public CommentsController(CommentContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult GetCommentList()
+        {
+            var values = _context.UserCommnets.ToList();
+            return Ok(values);
+        }
+
+        [HttpGet("GetOneCommentById/{id}")]
+        public IActionResult GetOneCommentById(int id)
+        {
+            var values = _context.UserCommnets.Find(id);
+            return Ok(values);
+        }
+
+
+        [HttpPost]
+        public IActionResult CreateCommand([FromBody]UserComment userComment)
+        {
+            _context.UserCommnets.Add(userComment); 
+            _context.SaveChanges();
+            return Ok("Yorum başarıyla eklendi");
+        }
+
+        [HttpPut]
+        public IActionResult UpdateCommand([FromBody] UserComment userComment)
+        {
+            _context.UserCommnets.Update(userComment);
+            _context.SaveChanges();
+            return Ok("Yorum başarıyla güncellendi");
+        }
+
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteCommand(int id)
+        {
+            var value = _context.UserCommnets.Find(id);
+            _context.UserCommnets.Remove(value);
+            _context.SaveChanges();
+            return Ok("Yorum başarıyla silindi");
+        }
+
+    }
+}
