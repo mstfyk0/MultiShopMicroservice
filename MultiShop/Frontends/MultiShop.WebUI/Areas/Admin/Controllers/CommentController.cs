752c9880846cb9e2132d60fdd7542ffe3c751977
Mustafa Yiğit KARAKOCA <ygt99krk@gmail.com>
2024-06-07 16:24:11 +0300

    Added of UserComment structure in webUI

---
+++ MultiShop/Frontends/MultiShop.WebUI/Areas/Admin/Controllers/CommentController.cs
@@ -0,0 +1,103 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using MultiShop.DtoLayer.CommentDtos;
+using Newtonsoft.Json;
+using System.Text;
+
+namespace MultiShop.WebUI.Areas.Admin.Controllers
+{
+    [AllowAnonymous]
+    [Area("Admin")]
+    [Route("Admin/Comment")]
+    public class CommentController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public CommentController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        [Route("Index")]
+        public async Task<IActionResult> Index()
+        {
+
+            ViewBag.v0 = "Yorum İşlemleri";
+            ViewBag.v1 = "Ana Sayfa";
+            ViewBag.v2 = "Yorumlar";
+            ViewBag.v3 = "Yorum Listesi";
+
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7053/api/comments/getAllComment");
+
+
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultCommentDto>>(jsonData);
+                return View(values);
+            }
+
+            return View();
+        }
+
+        [Route("delete/{id}")]
+        public async Task<IActionResult> Delete(string id)
+        {
+
+            var client = _httpClientFactory.CreateClient();
+            var responsMessage = await client.DeleteAsync("https://localhost:7053/api/comments/deleteComment/" + id);
+
+            if (responsMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index", "Comment", new { area = "Admin" });
+
+            }
+
+            return View();
+        }
+
+        [Route("update/{id}")]
+        public async Task<IActionResult> Update(string id)
+        {
+            ViewBag.v0 = "Yorum İşlemleri";
+            ViewBag.v1 = "Ana Sayfa";
+            ViewBag.v2 = "Yorumler";
+            ViewBag.v3 = "Yorum Güncelleme";
+
+
+            var client = _httpClientFactory.CreateClient();
+            var responsMessage = await client.GetAsync("https://localhost:7053/api/comments/getCommentById/" + id);
+
+
+            if (responsMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responsMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<UpdateCommentDto>(jsonData);
+                return View(values);
+            }
+
+            return View();
+        }
+
+
+        [HttpPost]
+        [Route("updateComment/{id}")]
+        public async Task<IActionResult> UpdateComment(UpdateCommentDto updateCommentDto)
+        {
+
+            var client = _httpClientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(updateCommentDto);
+            StringContent content = new StringContent(jsonData, Encoding.UTF8, "application/json");
+            var responsMessage = await client.PutAsync("https://localhost:7053/api/comments/updateComment", content);
+
+            if (responsMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index", "Comment", new { area = "Admin" });
+
+            }
+            return View();
+        }
+
+    }
+}
